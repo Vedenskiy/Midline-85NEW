1f56f3e7ec02fa606fffff53bd176e291a2bb8c6
scrawach <scrawach@gmail.com>
2024-10-11 10:01:21 +0300

    added FluentAssertions dependency to editor tests, write first not working test for Pipeline

---
+++ src/Call911/Assets/Tests/EditMode/Features/Calls/Infrastructure/PipelineTests.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeBase.Features.Calls.Infrastructure;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Tests.EditMode.Features.Calls.Infrastructure
+{
+    public class PipelineTests
+    {
+        [Test]
+        public async Task WhenExecuteNode_WithoutHandler_ThenShouldThrowException()
+        {
+            // assign
+            var handlers = new Dictionary<Type, IRequestHandler>();
+            var pipeline = new Pipeline(handlers);
+            
+            // act
+            Func<Task> act = async () => await pipeline.Execute(new Node());
+
+            // assert
+            await act.Should().NotThrowAsync();
+        }
+    }
+}
