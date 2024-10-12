7bb5d58828b3b03b8765db3d283c381bda2a821d
scrawach <scrawach@gmail.com>
2024-10-13 00:00:31 +0300

    added some test phrases

--- src/Call911/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ src/Call911/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using CodeBase.Features.Calls.Handlers.Phrases;
 using CodeBase.Features.Calls.Infrastructure.Handlers;
 using Reflex.Attributes;
@@ -15,8 +16,16 @@ namespace CodeBase.Infrastructure
 
         private async void Start()
         {
-            await _executor.Execute(new PhraseData()
-                { PersonKey = "Elena", MessageKey = "Hello, World!", DurationInSeconds = 2 });
+            foreach (var phraseData in GetTestPhrases()) 
+                await _executor.Execute(phraseData);
+        }
+
+        private IEnumerable<PhraseData> GetTestPhrases()
+        {
+            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Hello, world!", DurationInSeconds = 2 };
+            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Second message!", DurationInSeconds = 2 };
+            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Third message!", DurationInSeconds = 2 };
+            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Oops, it's all!", DurationInSeconds = 2 };
         }
     }
 }
