6d2f6e6c988dc96e9e916f6097859f0d83dee1dd
scrawach <scrawach@gmail.com>
2024-10-14 16:45:23 +0300

    used CallsExecutor for tests nodes with guids instead of Pipeline class

--- src/Call911/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ src/Call911/Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using CodeBase.Features.Calls.Handlers.Choices;
 using CodeBase.Features.Calls.Handlers.Phrases;
+using CodeBase.Features.Calls.Infrastructure;
 using CodeBase.Features.Calls.Infrastructure.Handlers;
 using CodeBase.Features.Calls.Infrastructure.Nodes;
 using Reflex.Attributes;
@@ -10,36 +11,45 @@ namespace CodeBase.Infrastructure
 {
     public class GameBootstrapper : MonoBehaviour
     {
-        private Pipeline _executor;
+        private CallsExecutor _executor;
+        private NodeRepository _nodes;
         
         [Inject]
-        public void Construct(Pipeline executor) => 
+        public void Construct(CallsExecutor executor, NodeRepository nodes)
+        {
             _executor = executor;
+            _nodes = nodes;
+        }
 
         private async void Start()
         {
-            foreach (var phraseData in GetTestPhrases()) 
-                await _executor.Execute(phraseData);
+            _nodes.Load(GetTestPhrases(), GetTestLinks());
+            await _executor.Execute("0");
+        }
+
+        private IEnumerable<NodeLink> GetTestLinks()
+        {
+            yield return Link("0", "1");
+            yield return Link("1", "2");
+            yield return Link("2", "3");
+            yield return Link("2", "4");
         }
 
         private IEnumerable<Node> GetTestPhrases()
         {
-            yield return new ChoicesData()
-            {
-                Choices = new List<ChoiceData>() { new() { ChoiceId = "1" }, new() { ChoiceId = "2" }, new() { ChoiceId = "3" } }
-            };
-            
-            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Hello, world!", DurationInSeconds = 2 };
-            yield return new PhraseData() { PersonKey = "Mark", MessageKey = "Hello, Elena!", DurationInSeconds = 2 };
-            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Second message!", DurationInSeconds = 2 };
-            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Third message!", DurationInSeconds = 2 };
-            
-            yield return new ChoicesData()
-            {
-                Choices = new List<ChoiceData>() { new() { ChoiceId = "4" }, new() { ChoiceId = "5" }, new() { ChoiceId = "6" } }
-            };
-            
-            yield return new PhraseData() { PersonKey = "Elena", MessageKey = "Oops, it's all!", DurationInSeconds = 2 };
+            yield return ElenaSay("Hello, World!", "0");
+            yield return MarkSay("Hello, Elena!", "1");
+            yield return ElenaSay("How are you?", "2");
+            yield return MarkSay("I'm fine, how you?", "3");
         }
+
+        private NodeLink Link(string parent, string child) => 
+            new() { ParentId = parent, ChildId = child };
+
+        private Node ElenaSay(string message, string withId) => 
+            new PhraseData() { Guid = withId, PersonKey = "Elena", MessageKey = message, DurationInSeconds = 2f };
+
+        private Node MarkSay(string message, string withId) =>
+            new PhraseData() { Guid = withId, PersonKey = "Mark", MessageKey = message, DurationInSeconds = 2f };
     }
 }
