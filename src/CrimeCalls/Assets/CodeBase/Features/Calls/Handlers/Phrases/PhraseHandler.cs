4af0701740c4e2676fe124c4b64d05bb25f7d2c8
scrawach <scrawach@gmail.com>
2024-11-02 12:08:09 +0300

    added extra pause duration to phrase handler, logging all shown phrases from phrase service

--- src/CrimeCalls/Assets/CodeBase/Features/Calls/Handlers/Phrases/PhraseHandler.cs
+++ src/CrimeCalls/Assets/CodeBase/Features/Calls/Handlers/Phrases/PhraseHandler.cs
@@ -1,11 +1,13 @@
 using System.Threading;
 using CodeBase.Features.Calls.Infrastructure.Handlers;
 using Cysharp.Threading.Tasks;
+using UnityEngine;
 
 namespace CodeBase.Features.Calls.Handlers.Phrases
 {
     public class PhraseHandler : RequestHandler<PhraseNode>
     {
+        private const float TechnicalPauseDuration = 2f;
         private readonly PhraseService _phrases;
 
         public PhraseHandler(PhraseService phrases) => 
@@ -19,7 +21,7 @@ namespace CodeBase.Features.Calls.Handlers.Phrases
         }
 
         private static UniTask AfterPhrasePause(PhraseNode request, CancellationToken token) => 
-            UniTask.Delay(FromSecondToMilliseconds(request.DurationInSeconds), cancellationToken: token);
+            UniTask.Delay(FromSecondToMilliseconds(request.DurationInSeconds + TechnicalPauseDuration), cancellationToken: token);
 
         private static int FromSecondToMilliseconds(float seconds) => 
             (int)(seconds * 1000);
--- src/CrimeCalls/Assets/CodeBase/Features/Calls/Handlers/Phrases/PhraseService.cs
+++ src/CrimeCalls/Assets/CodeBase/Features/Calls/Handlers/Phrases/PhraseService.cs
@@ -1,14 +1,24 @@
 ﻿using System;
+using CodeBase.Infrastructure.Common.Localization;
+using UnityEngine;
 
 namespace CodeBase.Features.Calls.Handlers.Phrases
 {
     public class PhraseService
     {
-        public event Action<PhraseNode> PhraseShown;
-        public event Action<PhraseNode> PhraseHide; 
+        private readonly LocalizationService _localization;
 
-        public void ShowPhrase(PhraseNode node) => 
+        public PhraseService(LocalizationService localization) => 
+            _localization = localization;
+
+        public event Action<PhraseNode> PhraseShown;
+        public event Action<PhraseNode> PhraseHide;
+
+        public void ShowPhrase(PhraseNode node)
+        {
+            Debug.Log($"[phrase] {node.PersonKey} say: [{node.MessageKey}] {_localization.GetTranslatedString(node.MessageKey)} ({node.DurationInSeconds}");
             PhraseShown?.Invoke(node);
+        }
 
         public void HidePhrase(PhraseNode node) =>
             PhraseHide?.Invoke(node);
